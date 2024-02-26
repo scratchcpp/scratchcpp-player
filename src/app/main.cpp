efd4c3fec5208a047f0189b7c279ad6df310081b
adazem009 <68537469+adazem009@users.noreply.github.com>
2024-02-26 18:56:35 +0100

    Register the uicomponents module

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "app.h"
-#include "globalmodule.h"
+#include "uicomponents/uicomponentsmodule.h"
 #include "keyboard/keyboardmodule.h"
 
 using namespace scratchcpp;
@@ -9,7 +9,7 @@ using namespace scratchcpp;
 int main(int argc, char *argv[])
 {
     App app;
-    app.addModule(new GlobalModule);
+    app.addModule(new uicomponents::UiComponentsModule);
     app.addModule(new keyboard::KeyboardModule);
 
     return app.run(argc, argv);
