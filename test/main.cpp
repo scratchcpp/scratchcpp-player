bf496a7406e549167dccc6df94197ad4fdf1d7c0
adazem009 <68537469+adazem009@users.noreply.github.com>
2024-02-28 19:34:29 +0100

    Use QApplication in tests

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,9 @@
-#include <QGuiApplication>
+#include <QApplication>
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv)
 {
-    QGuiApplication a(argc, argv);
+    QApplication a(argc, argv);
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
